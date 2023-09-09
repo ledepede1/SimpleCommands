30af797d1e32f745081cb736ebc20fdac26d762c
ledepede1 <142742362+ledepede1@users.noreply.github.com>
2023-09-09 23:23:32 +0200

    Delete SimpleCommands directory

--- SimpleCommands/Client/Notifications.lua
+++
@@ -1,7 +0,0 @@
-RegisterNetEvent('simpe_commands:Notifications') -- This Event only works with swt notification type Icon...
-AddEventHandler('simpe_commands:Notifications', function(swt_notificationsType, message,position,timeout,color,textColor,progress,icon)
-
-    TriggerEvent(swt_notificationsType, message,position,timeout,color,textColor,progress,icon);
-
-end)
-
